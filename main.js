4e53232c83cef980fb90c5a0305177c51dddfcd2
Francisco <franciscoserrano.pda@gmail.com>
2026-07-05 17:21:24 -0400

    flex wrap a proyectos

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 function revealOnScroll(){
-    const elements = document.querySelectorAll('.reveal'); 
+    let elements = document.querySelectorAll('.reveal'); 
 
     elements.forEach(el => {
-        const rect = el.getBoundingClientRect();
-        const isVisible = rect.top < window.innerHeight - 50;
+        let rect = el.getBoundingClientRect();
+        let isVisible = rect.top < window.innerHeight - 50;
 
         if(isVisible) {
             el.classList.add('active');
@@ -15,15 +15,15 @@ window.addEventListener('scroll', revealOnScroll);
 window.addEventListener('load', revealOnScroll);
 
 function copiarTexto() {
-    const texto = document.getElementById("textoCopiado").innerText;
+    let texto = document.getElementById("textoCopiado").innerText;
     navigator.clipboard.writeText(texto).then(() => {
       alert("Texto copiado: " + texto);
     });
   }
-  
-    const toggleBtn = document.querySelector('.menu-toggle');
-    const sidebar = document.querySelector('.sidebar');
 
-    toggleBtn.addEventListener('click', () => {
-    sidebar.classList.toggle('active');
-  });
+let toggleBtn = document.querySelector('.menu-toggle');
+let sidebar = document.querySelector('.sidebar');
+
+toggleBtn.addEventListener('click', () => {
+sidebar.classList.toggle('active');
+});
